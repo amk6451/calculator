bd2d7c2abdee6affaaa62a917581feb2239d76df
Alan Kwong <akwong910@gmail.com>
2023-02-21 19:42:54 -0500

    Updated JS file for operations

---
+++ calc.js
@@ -0,0 +1,40 @@
+function add(num1, num2){
+    return num1+num2
+};
+
+function subtract(num1, num2){
+    return num1-num2
+};
+
+function multiply(num1, num2){
+    return num1*num2
+};
+
+function divide(num1, num2){
+    return num1/num2
+};
+
+function operate(num1, num2, opChar){
+    switch (opChar) 
+    {
+        case "+":
+          return add(num1, num2);
+          break;
+
+        case "-":
+            return subtract(num1, num2);
+            break;
+
+        case "*":
+            return multiply(num1, num2);
+            break;
+
+        case "/":
+            return divide(num1, num2);
+            break;
+
+          default:
+            console.log("default");
+            break;
+    };
+};
